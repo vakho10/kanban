96f494269cdd3614ec685c6a8b780d0ed2c00bbf
vakho10 <vakho10@gmail.com>
2025-06-10 09:22:19 +0400

    Create starter template project

---
+++ src/test/java/ge/edu/sangu/kanban/KanbanApplicationTests.java
@@ -0,0 +1,13 @@
+package ge.edu.sangu.kanban;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
